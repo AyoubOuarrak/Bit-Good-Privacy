b46a4f7d8c082a74b1cefd1f15fcf2e4a86cbda2
AyoubOuarrak <andreyvondorcon@gmail.com>
2015-05-08 17:18:42 +0200

    Added method to convert secret keys in string e viceversa

---
+++ src/com/bgp/keymanager/SessionKeyManager.java
@@ -0,0 +1,35 @@
+package com.bgp.keymanager;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ * Class that manipulate session keys
+ * @author ayoub
+ *
+ */
+public class SessionKeyManager {
+
+    /**
+     * Convert string to session key
+     * @param key string key
+     * @return secret key object
+     */
+    public static SecretKey convertToKey(String key) {
+        byte[] encodedKey = Base64.decodeBase64(key);
+        SecretKey sessionKey = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
+        return sessionKey;
+    }
+    
+    /**
+     * Convert session key to string
+     * @param key
+     * @return
+     */
+    public static String convertToString(SecretKey key) {
+        byte[] data = key.getEncoded();
+        String sessionKey = Base64.encodeBase64String(data);
+        return sessionKey;
+    }
+}
